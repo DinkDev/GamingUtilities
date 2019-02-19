4cb02b7a7ed7634d64b4f29b33598f8c33a1c212
Dale <dinkhome@hotmail.com>
2019-02-18 20:30:45 -0500

    Script for extracting images from PDFs (for use as tokens).

---
+++ PowerShell/ExtractImages.ps1
@@ -0,0 +1,32 @@
+param (
+  [string]$pdfFile = 'D:\temp\pdftest\VASTEC_CapState_2015_R.pdf'
+)
+
+$pdfInfoExe = 'C:\ProgramData\chocolatey\bin\pdfinfo.exe'
+$pdfImagesExe = 'C:\ProgramData\chocolatey\bin\pdfimages.exe'
+
+$pdfInfo = & $pdfInfoExe $pdfFile
+
+$pdfPagesResult = $pdfInfo | Where-Object {$_ -match 'Pages:'} |
+  Foreach-Object { $_ -replace 'Pages:\s*', ''} |
+  Select-Object -First 1
+
+[int]$pages = [convert]::ToInt32($pdfPagesResult, 10)
+
+# Write-Output "The file $pdfFile has $pages pages."
+Write-Output "The file $pdfFile has $pages pages."
+
+$baseDir = [io.path]::GetFileNameWithoutExtension($pdfFile)
+
+$pageDigits = [math]::Truncate([math]::Log10($pages)) + 1;
+
+Write-Output "log10 of pages = $pageDigits"
+
+For ($page = 1; $page -le $pages; $page++) {
+  $targetDir = $baseDir + '.page' + $page.ToString().Padleft($pageDigits, 0)
+  $targetOutput = $targetDir + '\image'
+
+  New-Item -ItemType directory -Path $targetDir
+
+  & $pdfImagesExe -f $page -l $page $pdfFile $targetOutput
+}
